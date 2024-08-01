4213a9b38a0b9000e9149839e843df464b5419d4
Centy26 <centrineatieno20@gmail.com>
2024-08-02 00:59:02 +0300

    Event Handling in React

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,22 @@
 // Code EyesOnMe Component Here
+import React from "react";
+
+function EyesOnMe () {
+    const handleFocus= () => {console.log("Good!")};
+
+    const handleBlur = () => {console.log("Hey! Eyes on me!")};
+    return (
+        <div>
+            {/* attach the events to the button */}
+            <button onFocus = {handleFocus}  onBlur = {handleBlur}>Eyes on me</button>
+        </div>
+    )
+}
+
+
+
+
+
+
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,15 @@
 // Code Keypad Component Here
+import React from "react";
+
 
 function Keypad (){
+    // define the function for the event handler after writing the input
+    const handleChange = (event) => {console.log ("Entering password...")};
     return (
-        <div></div>
+        <div>
+            {/* after defining the event handler function, i will attach the onChange event to the input */}
+            <input type= "password"  onChange= {handleChange} />
+        </div>
     )
 }
 
